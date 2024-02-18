5c7364179eee7eb6db34c52fccbc74f2c485da06
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2024-02-18 17:26:06 +0200

    dev: fixed return type and missing inheritance

--- app/src/main/java/com/example/fitnesstracker/data/daos/TestDao.kt
+++ app/src/main/java/com/example/fitnesstracker/data/daos/TestDao.kt
@@ -1,12 +1,13 @@
 package com.example.fitnesstracker.data.daos
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
 import com.example.fitnesstracker.data.entities.TestEntity
 
 @Dao
-interface TestDao
+interface TestDao: IDao<TestEntity>
 {
     @Query("SELECT * FROM tests")
-    fun getAll(): List<TestEntity>
+    fun getAll(): LiveData<List<TestEntity>>
 }
