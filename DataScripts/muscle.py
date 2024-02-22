0f870ea95df5bfb202211665408558bd1d575988
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2024-02-22 12:22:50 +0200

    data: created entity

--- DataScripts/muscle.py
+++ DataScripts/muscle.py
@@ -5,3 +5,9 @@ class Muscle:
     def __str__(self):
         return self.name
 
+class MuscleEntity:
+    idCount = 0
+    def __init__(self, muscle: Muscle):
+        self.id = MuscleEntity.idCount
+        MuscleEntity.idCount += 1
+        self.name = muscle.name
