f6baa2822fa24985566e86cb791701a09718421e
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2024-02-22 16:03:08 +0200

    dev: seed db exercises table

--- app/src/main/java/com/example/fitnesstracker/data/Seeder.kt
+++ app/src/main/java/com/example/fitnesstracker/data/Seeder.kt
@@ -1,6 +1,7 @@
 package com.example.fitnesstracker.data
 
 import android.content.Context
+import com.example.fitnesstracker.data.entities.ExerciseEntity
 import com.example.fitnesstracker.data.entities.MuscleEntity
 import com.example.fitnesstracker.utils.fromJson
 import com.example.fitnesstracker.utils.readJsonFromAssets
@@ -10,10 +11,26 @@ class Seeder (
     private val database: AppDatabase
 ) {
     private val musclesPath = "muscles.json"
+    private val exercisesPath = "exercises.json"
 
     fun seed() {
         val muscles = loadMuscles()
         insertMuscles(muscles)
+        val exercises = loadExercises()
+        insertExercises(exercises)
+    }
+
+    private fun loadExercises(): Array<ExerciseEntity> {
+        val jsonString = readJsonFromAssets(context, exercisesPath)
+        val exercises: Array<ExerciseEntity> = fromJson(jsonString)
+        return exercises
+    }
+
+    private fun insertExercises(exercises: Array<ExerciseEntity>) {
+        val exerciseDao = database.exerciseDao()
+        exercises.forEach {
+            exerciseDao.insert(it)
+        }
     }
     private fun loadMuscles(): Array<MuscleEntity> {
         val jsonString = readJsonFromAssets(context, musclesPath)
