b8dcd82f44d21a95b9702a181974322d54fff5ce
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2024-02-22 12:23:08 +0200

    data: printing muscles and exercises to json file

--- DataScripts/main.py
+++ DataScripts/main.py
@@ -1,6 +1,23 @@
-from excelReader import ExcelReader
+import json
 
+from excelReader import ExcelReader
+from exercise import ExerciseEntity
+from jsonEncoder import JsonEncoder
+from muscle import MuscleEntity
+
+
+outputPath = '../app/src/main/assets/'
 reader = ExcelReader('../data/db-preload.xlsx')
 
-print(len(reader.exercises))
+
+muscles = [MuscleEntity(muscle) for muscle in reader.muscles]
+exercises = [ExerciseEntity(exercise) for exercise in reader.exercises]
+
+
+
+
+with open(outputPath + 'muscles.json', 'w') as file:
+    json.dump(muscles, file, cls=JsonEncoder, indent=2)
+with open(outputPath + 'exercises.json', 'w') as file:
+    json.dump(exercises, file, cls=JsonEncoder, indent=2)
 
