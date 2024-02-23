33d57ba83dc5eb41fdc8dba284cdaf949aa20660
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2024-02-23 09:45:35 +0200

    dev: fixed type weight not displaying properly

--- app/src/main/java/com/example/fitnesstracker/ui/theme/Type.kt
+++ app/src/main/java/com/example/fitnesstracker/ui/theme/Type.kt
@@ -2,16 +2,24 @@ package com.example.fitnesstracker.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.fitnesstracker.R
 import javax.annotation.concurrent.Immutable
 
+@OptIn(ExperimentalTextApi::class)
 val JosefineFontFamily = FontFamily(
-    Font(R.font.josefine_sans_variable_wght)
+    Font(
+        R.font.josefine_sans_variable_wght,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(FontWeight.Normal.weight),
+        )
+    )
 )
 
 @Immutable
