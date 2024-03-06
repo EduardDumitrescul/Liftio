77a7808b798da8e525991346ad54ef930ec994bb
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2024-03-06 16:48:45 +0200

    dev: centered app bar (sync)

--- app/src/main/java/com/example/fitnesstracker/ui/components/CenteredAppBar.kt
+++ app/src/main/java/com/example/fitnesstracker/ui/components/CenteredAppBar.kt
@@ -1,4 +1,20 @@
 package com.example.fitnesstracker.ui.components
 
-class CenteredAppBar {
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CenteredAppBar(
+    modifier: Modifier = Modifier,
+    title: String
+) {
+    CenterAlignedTopAppBar(title = { /*TODO*/ })
+}
+
+@Composable
+@Preview
+fun CenteredAppBarPreview() {
+
 }
