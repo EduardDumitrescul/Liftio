55645458b0a2caaa4bd076418d1d421dac6ffd2a
Eduard-Valentin Dumitrescul <dumitresculeduard@gmail.com>
2024-06-25 08:54:56 +0300

    fix: container color and width

--- app/src/main/java/com/example/fitnesstracker/view/components/textfield/FilledTextField.kt
+++ app/src/main/java/com/example/fitnesstracker/view/components/textfield/FilledTextField.kt
@@ -1,18 +1,15 @@
 package com.example.fitnesstracker.view.components.textfield
 
-import android.util.Log
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,8 +17,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.input.VisualTransformation
@@ -44,7 +39,7 @@ fun FilledTextField(
             text = it
             onValueChange(it)
         },
-        modifier = Modifier.height(40.dp),
+        modifier = Modifier.height(40.dp).fillMaxWidth(),
         cursorBrush = SolidColor(AppTheme.colors.primary),
         enabled = true,
         interactionSource = interactionSource
@@ -79,6 +74,8 @@ fun FilledTextField(
                 unfocusedLeadingIconColor = AppTheme.colors.primaryVariant,
                 unfocusedPlaceholderColor = AppTheme.colors.primaryVariant,
                 focusedPlaceholderColor = Color.Transparent,
+                unfocusedContainerColor = AppTheme.colors.container,
+                focusedContainerColor = AppTheme.colors.container,
             ),
             contentPadding = TextFieldDefaults.contentPaddingWithoutLabel(
                 top = 0.dp,
