30f27fdad0fbabf27533991765b2872090e0a4ba
Eduard-Valentin Dumitrescul <dumitresculeduard@gmail.com>
2024-06-25 10:04:36 +0300

    ref: typo

--- app/src/main/java/com/example/fitnesstracker/view/exercise/ExerciseListView.kt
+++ app/src/main/java/com/example/fitnesstracker/view/exercise/ExerciseListView.kt
@@ -29,7 +29,7 @@ fun ExerciseListView(
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         item {
-            LargeAppBar(title = "Exerqqcises")
+            LargeAppBar(title = "Exercises")
         }
         item {
             FilledTextField(
