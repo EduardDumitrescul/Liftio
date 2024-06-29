282441fcfe9b3742ac37891b174de7cf73808145
Eduard-Valentin Dumitrescul <dumitresculeduard@gmail.com>
2024-06-29 16:36:30 +0300

    fix: bottomnavabar overlapping fab

--- app/src/main/java/com/example/fitnesstracker/MainActivity.kt
+++ app/src/main/java/com/example/fitnesstracker/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
@@ -42,8 +43,11 @@ class MainActivity : ComponentActivity() {
                             bottomNavBarDestinations.toList(),
                             navController = navController)
                     }
-                ) {
-                    AppNavHost(navController = navController)
+                ) { innerPaddings ->
+                    AppNavHost(
+                        navController = navController,
+                        modifier = Modifier.padding(innerPaddings)
+                    )
                 }
             }
         }
