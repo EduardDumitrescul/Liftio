ba3fa3eee057fb394385a1482dc25fc379873fff
Eduard-Valentin Dumitrescul <dumitresculeduard@gmail.com>
2024-06-29 10:21:31 +0300

    feat: top app bar scrolling and alignment

--- app/src/main/java/com/example/fitnesstracker/view/components/appbar/LargeAppBar.kt
+++ app/src/main/java/com/example/fitnesstracker/view/components/appbar/LargeAppBar.kt
@@ -1,12 +1,20 @@
 package com.example.fitnesstracker.view.components.appbar
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.exclude
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.fitnesstracker.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -14,18 +22,19 @@ fun LargeAppBar(
     title: String,
     modifier: Modifier = Modifier,
     onNavigationIconClick: () -> Unit = {},
+    scrollBehavior: TopAppBarScrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 ) {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
-    TopAppBar(
+    MediumTopAppBar(
         modifier = modifier,
         colors = appBarColors,
         title = { AppBarTitle(title) },
         navigationIcon = { BackNavigationIcon(onClick = onNavigationIconClick) },
         actions = { MoreActionsIcon(onClick = { /*TODO*/ })},
-        scrollBehavior = scrollBehavior
+        scrollBehavior = scrollBehavior,
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 private fun LargeAppBarPreview() {
--- app/src/main/java/com/example/fitnesstracker/view/exercise/ExerciseListView.kt
+++ app/src/main/java/com/example/fitnesstracker/view/exercise/ExerciseListView.kt
@@ -1,13 +1,20 @@
 package com.example.fitnesstracker.view.exercise
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.fitnesstracker.view.components.appbar.LargeAppBar
@@ -16,33 +23,53 @@ import com.example.fitnesstracker.view.theme.AppTheme
 import androidx.hilt.navigation.compose.hiltViewModel
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExerciseListView(
     navigateBack: () -> Unit,
     viewModel: ExerciseListViewModel = hiltViewModel<ExerciseListViewModel>(),
 ) {
     val exerciseSummaries by viewModel.filteredExerciseSummaries.collectAsState()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
 
-    LazyColumn(
-        Modifier.padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        item {
+    Scaffold(
+        modifier = Modifier
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
             LargeAppBar(
                 title = "Exercises",
-                onNavigationIconClick = navigateBack
+                onNavigationIconClick = navigateBack,
+                scrollBehavior = scrollBehavior
             )
         }
-        item {
-            FilledTextField(
-                onValueChange = {viewModel.updateFilter(it)},
-                placeholderText = "exercise name"
-            )
-        }
-
-
-        items(exerciseSummaries) {
-            ExerciseRow(it)
+    ) {innerPadding ->
+        LazyColumn(
+            Modifier.padding(bottom = innerPadding.calculateBottomPadding())
+                .padding(top = innerPadding.calculateTopPadding())
+                .padding(horizontal = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            item {
+                FilledTextField(
+                    onValueChange = {viewModel.updateFilter(it)},
+                    placeholderText = "exercise name"
+                )
+            }
+            items(exerciseSummaries) {
+                ExerciseRow(it)
+            }
+            items(exerciseSummaries) {
+                ExerciseRow(it)
+            }
+            items(exerciseSummaries) {
+                ExerciseRow(it)
+            }
+            items(exerciseSummaries) {
+                ExerciseRow(it)
+            }
+            items(exerciseSummaries) {
+                ExerciseRow(it)
+            }
         }
     }
 }
