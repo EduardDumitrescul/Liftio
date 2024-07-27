767a0366d3925ed1dd510b1890862a8d02cf1ca3
Eduard Dumitrescul <dumitresculeduard@gmail.com>
2024-07-27 10:12:13 +0300

    fix: alignment

--- app/src/main/java/com/example/fitnesstracker/ui/views/template/edit/EditableSetRow.kt
+++ app/src/main/java/com/example/fitnesstracker/ui/views/template/edit/EditableSetRow.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,6 +35,7 @@ fun EditableSetRow(
 
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .background(Color.Transparent)
             .then(modifier)
