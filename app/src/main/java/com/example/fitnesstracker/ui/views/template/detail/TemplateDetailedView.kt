3755dcc509487eb5d97f149f75e3dc139e816ab2
Eduard Dumitrescul <dumitresculeduard@gmail.com>
2024-08-03 12:34:09 +0300

    feat: remove template button

--- app/src/main/java/com/example/fitnesstracker/ui/views/template/detail/TemplateDetailedView.kt
+++ app/src/main/java/com/example/fitnesstracker/ui/views/template/detail/TemplateDetailedView.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -36,6 +37,12 @@ fun TemplateDetailedView(
             LargeAppBar(
                 title = templateWithExercises.template.name,
                 actions = {
+                    IconButton(onClick = {
+                        navigateBack()
+                        viewModel.removeTemplate()
+                    }) {
+                        Icon(Icons.Rounded.Delete, "remove template")
+                    }
                     IconButton(onClick ={
                         navigateToTemplateEditView(templateWithExercises.template.id)
                         }
--- app/src/main/java/com/example/fitnesstracker/ui/views/template/detail/TemplateDetailedViewModel.kt
+++ app/src/main/java/com/example/fitnesstracker/ui/views/template/detail/TemplateDetailedViewModel.kt
@@ -5,17 +5,46 @@ import androidx.lifecycle.viewModelScope
 import com.example.fitnesstracker.data.dto.TemplateDetailed
 import com.example.fitnesstracker.services.TemplateService
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TemplateDetailedViewModel @Inject constructor(
     val templateId: Int,
-    templateService: TemplateService
-): ViewModel() {
-    val templateDetailed: StateFlow<TemplateDetailed> = templateService
-        .getTemplateWithExercisesById(templateId)
-        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), TemplateDetailed.default())
+    private val templateService: TemplateService
+) : ViewModel() {
+
+    private val _templateDetailed = MutableStateFlow(TemplateDetailed.default())
+    val templateDetailed: StateFlow<TemplateDetailed> get() = _templateDetailed
+
+    private var collectionJob: Job? = null
+
+    init {
+        startCollectingTemplateData()
+    }
+
+    private fun startCollectingTemplateData() {
+        collectionJob?.cancel() // Cancel any previous collection
+        collectionJob = viewModelScope.launch {
+            templateService.getTemplateWithExercisesById(templateId)
+                .collect { templateDetailed ->
+                    _templateDetailed.value = templateDetailed
+                }
+        }
+    }
+
+    fun removeTemplate() {
+        viewModelScope.launch(Dispatchers.IO) {
+            templateService.removeTemplate(templateId)
+            stopCollectingTemplateData()
+        }
+    }
+
+    private fun stopCollectingTemplateData() {
+        collectionJob?.cancel()
+    }
 }
