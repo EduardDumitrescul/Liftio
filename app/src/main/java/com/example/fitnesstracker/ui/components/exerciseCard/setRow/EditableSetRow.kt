4f5928a726c7198916803122f3c47d00b7d4ed7d
Eduard Dumitrescul <dumitresculeduard@gmail.com>
2024-08-04 18:27:16 +0300

    feat: impl map for managing row style

--- app/src/main/java/com/example/fitnesstracker/ui/components/exerciseCard/setRow/EditableSetRow.kt
+++ app/src/main/java/com/example/fitnesstracker/ui/components/exerciseCard/setRow/EditableSetRow.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,13 +44,19 @@ fun EditableSetRow(
     var isEditing by remember {
         mutableStateOf(false)
     }
+    val alpha = if(state.style == SetRowStyle.DISABLED) 0.5f else 1.0f
 
-    val backgroundColor = if(isEditing) AppTheme.colors.containerVariant else Color.Transparent
+    val backgroundColor = when {
+        state.style == SetRowStyle.HIGHLIGHTED ->  AppTheme.colors.containerVariant
+        isEditing ->  AppTheme.colors.containerVariant
+        else -> Color.Transparent
+    }
 
     Column(
         modifier = Modifier
             .background(backgroundColor)
             .fillMaxWidth()
+            .alpha(alpha)
             .then(modifier),
     ) {
         MainRow(
--- app/src/main/java/com/example/fitnesstracker/ui/views/workout/WorkoutOngoingViewModel.kt
+++ app/src/main/java/com/example/fitnesstracker/ui/views/workout/WorkoutOngoingViewModel.kt
@@ -3,7 +3,10 @@ package com.example.fitnesstracker.ui.views.workout
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.fitnesstracker.services.WorkoutService
+import com.example.fitnesstracker.ui.components.exerciseCard.setRow.SetRowStyle
 import com.example.fitnesstracker.ui.components.exerciseCard.setRow.SetState
+import com.example.fitnesstracker.ui.components.exerciseCard.setRow.toSetState
+import com.example.fitnesstracker.ui.components.exerciseCard.toExerciseCardState
 import com.example.fitnesstracker.ui.views.template.detail.WorkoutState
 import com.example.fitnesstracker.ui.views.template.detail.toWorkoutState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,7 +15,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -25,12 +28,10 @@ class WorkoutOngoingViewModel @Inject constructor(
     private val workoutId: Int,
     private val workoutService: WorkoutService
 ): ViewModel() {
-    val ongoingWorkout: StateFlow<WorkoutState> = workoutService
-        .getTemplateWithExercisesById(workoutId)
-        .map {
-            it.toWorkoutState()
-        }
-        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), WorkoutState.default())
+    private val rowStyleMapFlow: MutableStateFlow<Map<Int, SetRowStyle>> = MutableStateFlow(emptyMap())
+
+    private lateinit var _ongoingWorkout: StateFlow<WorkoutState>
+    val ongoingWorkout get() = _ongoingWorkout
 
     private val startTime = LocalDateTime.now()
     private val _elapsedTime = MutableStateFlow(0L)
@@ -38,8 +39,28 @@ class WorkoutOngoingViewModel @Inject constructor(
     private val timerRunning = true
 
     init {
+        fetchData()
         runTimer()
     }
+
+    private fun fetchData() {
+        val templateFlow = workoutService.getTemplateWithExercisesById(workoutId)
+        val flow = templateFlow.combine(rowStyleMapFlow) { templateDetailed, rowStyleMap ->
+            templateDetailed
+                .toWorkoutState()
+                .copy(
+                    exerciseCardStates = templateDetailed.exercisesWithSetsAndMuscles.map { detailedExercise ->
+                        detailedExercise.toExerciseCardState().copy(
+                            sets = detailedExercise.sets.map {
+                                it.toSetState(rowStyleMap[it.id] ?: SetRowStyle.DISABLED)
+                            }
+                        )
+                    }
+                )
+        }
+        _ongoingWorkout = flow.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), WorkoutState.default())
+    }
+
 
     private fun runTimer() {
         viewModelScope.launch {
