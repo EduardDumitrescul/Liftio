03dd7bafad67d02b54841d96930be76d792f7d01
Eduard-Valentin Dumitrescul <dumitresculeduard@gmail.com>
2024-08-23 14:58:15 +0300

    ref: changed icon color

--- app/src/main/java/com/example/fitnesstracker/ui/views/workout/perform/WorkoutPerformScreen.kt
+++ app/src/main/java/com/example/fitnesstracker/ui/views/workout/perform/WorkoutPerformScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.CheckCircle
 import androidx.compose.material.icons.rounded.CheckCircleOutline
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -133,7 +134,7 @@ private fun FinishButton(onClick: () -> Unit) {
         imageVector = Icons.Rounded.CheckCircleOutline,
         contentDescription = "finish workout",
         containerColor = Color.Transparent,
-        contentColor = AppTheme.colors.primary,
+        contentColor = AppTheme.colors.onBackground,
         size = 40.dp
     )
 }
